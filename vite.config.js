37e16fa0e6ec6877166caecaf8a5b3e2b37caf38
sheikh-musa <97861619+sheikh-musa@users.noreply.github.com>
2024-08-28 13:29:06 +0800

    gh-pages custom domain

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/one18-inventory/",
+  base: "/",
   build: {
     rollupOptions: {
       output: {
